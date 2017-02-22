4d0e67500019b1c0eedfd349ab2b9a239c6223b6
Lindsey Bieda <hazardous.narayan@gmail.com>
2017-02-21 21:39:49 -0500

    Update to support reading in messages from bluetooth

--- messangerbag.ino
+++ messangerbag.ino
@@ -1,18 +1,25 @@
 
+#include <SoftwareSerial.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_BLE.h>
+#include <Adafruit_BluefruitLE_SPI.h>
+#include <Adafruit_BluefruitLE_UART.h>
 #ifndef PSTR
  #define PSTR // Make Arduino Due happy
 #endif
 
-#define PIN 12
+#define MATRIX_PIN 12 // NM data pin
+#define RX_PIN    9   // BLE 'TXO' pin
+#define CTS_PIN   10  // BLE 'CTS' pin
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
+SoftwareSerial    ser(RX_PIN, -1);
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, MATRIX_PIN,
   NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
-  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-  NEO_GRB            + NEO_KHZ800);
-
+  NEO_MATRIX_COLUMNS    + NEO_MATRIX_ZIGZAG,
+  NEO_GRB               + NEO_KHZ800);
+  
 const uint16_t colors[] = {
   matrix.Color(255, 0, 0),    // red
   matrix.Color(255, 127, 0),  // orange
@@ -25,9 +32,9 @@ const uint16_t colors[] = {
 
 const int NUM_COLORS = 7;
 
-int x    = matrix.width();
-int y    = matrix.height();
-int pass = 0;
+int x                 = matrix.width();
+int pass              = 0;
+boolean gotNewMessage = false;
 
 String message = "<3 <3 <3";
 int messageLenCheck = message.length() * -6;
@@ -37,9 +44,29 @@ void setup() {
   matrix.setTextWrap(false);
   matrix.setBrightness(40);
   matrix.setTextColor(colors[0]);
+
+  pinMode(CTS_PIN, OUTPUT);
+  digitalWrite(CTS_PIN, HIGH);
+  ser.begin(9600);
 }
 
-void loop() {  
+void loop() {
+  int c;
+  String newMessage;
+    
+  digitalWrite(CTS_PIN, LOW); // okay to send data
+  while((c = ser.read()) > 0){
+    Serial.print(char(c));
+    gotNewMessage = true;
+    newMessage += char(c);
+  }
+  digitalWrite(CTS_PIN, HIGH);
+
+  if(gotNewMessage) {
+    message = newMessage;
+    gotNewMessage = false;
+  }
+
   scrollText();
 }
 
